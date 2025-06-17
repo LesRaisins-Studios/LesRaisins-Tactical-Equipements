455777c2008fc4ff5ed0fd0f93ceda111896b186
xjqsh <59763518+xjqsh@users.noreply.github.com>
2025-06-17 17:14:46 +0800

    将effect组改为正则

--- src/main/java/me/xjqsh/lrtactical/client/renderer/model/CustomBedrockModel.java
+++ src/main/java/me/xjqsh/lrtactical/client/renderer/model/CustomBedrockModel.java
@@ -1,20 +1,24 @@
 package me.xjqsh.lrtactical.client.renderer.model;
 
 import com.tacz.guns.client.model.BedrockAnimatedModel;
+import com.tacz.guns.client.model.FunctionalBedrockPart;
+import com.tacz.guns.client.model.IFunctionalRenderer;
 import com.tacz.guns.client.model.bedrock.BedrockPart;
 import com.tacz.guns.client.model.bedrock.ModelRendererWrapper;
 import com.tacz.guns.client.model.functional.LeftHandRender;
 import com.tacz.guns.client.model.functional.RightHandRender;
 import com.tacz.guns.client.resource.pojo.model.BedrockModelPOJO;
 import com.tacz.guns.client.resource.pojo.model.BedrockVersion;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import static com.tacz.guns.client.model.GunModelConstant.*;
 
 public class CustomBedrockModel extends BedrockAnimatedModel {
-    private static final String EFFECT_NODE = "effect";
-    private final BedrockPart effectNode;
+    private static final Pattern FIRSTPERSON_EFFECT_PATTERN = Pattern.compile("^1p_effect(_(\\d+))?$");
+    private boolean effectVisible = false;
 
     public CustomBedrockModel(BedrockModelPOJO pojo, BedrockVersion version) {
         super(pojo, version);
@@ -23,13 +27,22 @@ public class CustomBedrockModel extends BedrockAnimatedModel {
         // 右手手臂
         this.setFunctionalRenderer(RIGHTHAND_POS_NODE, bedrockPart -> new RightHandRender(this));
 
-        this.effectNode = Optional.ofNullable(modelMap.get(EFFECT_NODE)).map(ModelRendererWrapper::getModelRenderer).orElse(null);
-        this.setEffectVisible(false);
+        for (Map.Entry<String, ModelRendererWrapper> entry : modelMap.entrySet()) {
+            if (FIRSTPERSON_EFFECT_PATTERN.matcher(entry.getKey()).find()) {
+                if (entry.getValue().getModelRenderer() instanceof FunctionalBedrockPart functionalPart) {
+                    functionalPart.functionalRenderer = this::renderEffect;
+                }
+            }
+        }
+    }
+
+    @Nullable
+    private IFunctionalRenderer renderEffect(BedrockPart part) {
+        part.visible = effectVisible;
+        return null;
     }
 
     public void setEffectVisible(boolean visible) {
-        if (effectNode != null) {
-            effectNode.visible = visible;
-        }
+        this.effectVisible = visible;
     }
 }
